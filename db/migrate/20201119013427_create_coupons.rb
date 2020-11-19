2a32b6458725153a1e2b8d9a76adcd6a02162b07
Beka23 <beksultankaiypov@gmail.com>
2020-11-18 20:45:29 -0500

    commit tree 6b81489998c1ed19f6fece52c170337761309b4d

---
+++ db/migrate/20201119013427_create_coupons.rb
@@ -0,0 +1,12 @@
+class CreateCoupons < ActiveRecord::Migration[5.0]
+  def change
+    create_table :coupons do |t|
+      t.string :coupone_code
+      t.string :store
+      t.string :lab
+      t.string :%
+
+      t.timestamps
+    end
+  end
+end
